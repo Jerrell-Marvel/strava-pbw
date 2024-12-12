38a9d0363d0a09cdbf35773372f42b522b190484
Alief <6182201073@student.unpar.ac.id>
2024-12-12 11:29:02 +0700

    Front end template

---
+++ src/main/java/com/example/demo/Admin/Admin.java
@@ -0,0 +1,11 @@
+package com.example.demo.Admin;
+
+import lombok.Data;
+
+@Data
+public class Admin {
+    private int id_Member;
+    private String nama_member;
+    private String email;
+    private String password;
+}
